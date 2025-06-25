19bdbca9b55984463c2441c2463c09486969a211
Faisal Aljaberi <faljaberi126@gmail.com>
2025-06-25 22:01:58 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,18 @@
-from ComputeGCA import compute_geometric_contact_angle
-file_path = 'MW_fw50-SV.tif'
-fluidLabel = 1
-SolidLabel = 3
+"""
+Compute geometric contact angle from segmented image data.
 
-compute_geometric_contact_angle(file_path, fluidLabel=fluidLabel, solidLabel=SolidLabel, min_points=20)
+Output is txt file with contact angle values.
+"""
+from geometricCA import compute_geometric_contact_angle
+
+# Configuration
+file_path   = 'MW_fw50-SV.tif'
+fluid_Label = 1 # Light phase fluid, in case of brine-gas system (label is for gas phase)
+solid_Label = 2
+
+# Run computation
+compute_geometric_contact_angle(
+    file_path, 
+    fluidLabel=fluid_Label, 
+    solidLabel=solid_Label
+)
